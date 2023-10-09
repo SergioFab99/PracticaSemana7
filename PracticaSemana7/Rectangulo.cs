bdc3b3ce9316de8937c3855f97a74dc86a10128c
Sergio Fabián <sergioandrefabianvalverde@gmail.com>
2023-10-09 17:48:57 -0500

    rectangulo creado

---
+++ PracticaSemana7/Rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaSemana7
+{
+        class Rectangulo
+        {
+            private double baseRectangulo;
+            private double alturaRectangulo;
+
+            public Rectangulo(double baseRectangulo, double alturaRectangulo)
+            {
+                this.baseRectangulo = baseRectangulo;
+                this.alturaRectangulo = alturaRectangulo;
+            }
+
+            public double CalcularArea()
+            {
+                return baseRectangulo * alturaRectangulo;
+            }
+        }
+}
