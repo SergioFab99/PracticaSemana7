96b7ecb18e8332a3a7f942b22a83ea8695ce3c24
Sergio Fabián <sergioandrefabianvalverde@gmail.com>
2023-10-09 17:51:49 -0500

    clase circulo creada

---
+++ PracticaSemana7/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaSemana7
+{
+    class Circulo
+    {
+        private double radioCirculo;
+
+        public Circulo(double radioCirculo)
+        {
+            this.radioCirculo = radioCirculo;
+        }
+
+        public double CalcularArea()
+        {
+            return Math.PI * Math.Pow(radioCirculo, 2);
+        }
+    }
+}
